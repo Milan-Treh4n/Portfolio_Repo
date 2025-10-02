35a218453b454d04571a6fa1108dc1825600522c
Milan-Treh4n <trehanmilan5@gmail.com>
2025-10-02 14:00:06 +0100

    Generating sdynthetic data

---
+++ synthetic data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+# Parameters
+m, b = 2, 5
+x = np.linspace(0, 10, 100)
+noise = np.random.normal(0, 1, size=len(x))
+y = m * x + b + noise
+
+# Save to CSV
+df = pd.DataFrame({"x": x, "y": y})
+df.to_csv("data.csv", index=False)
